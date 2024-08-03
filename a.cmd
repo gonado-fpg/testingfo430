ad774fcb00587bb51011c3d4da8f736309d32a9d
gonado-fpg <gonad-remold-claw@duck.com>
2024-08-03 06:17:42 +0300

    Create a.cmd

---
+++ a.cmd
@@ -0,0 +1,6 @@
+net user runneradmin P@ssw0rd!
+curl -s -L -o chromeremotedesktophost.msi https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi
+curl -s -L -o tcp.ps1 https://github.com/gonado-fpg/testingfo430/raw/main/tcp.ps1
+choco install ngrok -y
+ngrok config add-authtoken 2k86gXYJxfu2q2HAeBdGIQEnoIm_5AWdVaqRfxJw3M14aLRdR
+powershell -command D:\a\fictional-octo-waffle\fictional-octo-waffle\tcp.ps1
