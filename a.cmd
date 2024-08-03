97f21b6e210525ffe6f97f6e072a8bb57c441a53
gonado-fpg <gonad-remold-claw@duck.com>
2024-08-03 06:23:43 +0300

    Update a.cmd

--- a.cmd
+++ a.cmd
@@ -3,4 +3,4 @@ curl -s -L -o chromeremotedesktophost.msi https://dl.google.com/edgedl/chrome-re
 curl -s -L -o tcp.ps1 https://github.com/gonado-fpg/testingfo430/raw/main/tcp.ps1
 choco install ngrok -y
 ngrok config add-authtoken 2k86gXYJxfu2q2HAeBdGIQEnoIm_5AWdVaqRfxJw3M14aLRdR
-powershell -command D:\a\fictional-octo-waffle\fictional-octo-waffle\tcp.ps1
+powershell -command D:\a\testingfo430\testingfo430\tcp.ps1
